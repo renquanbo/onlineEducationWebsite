ea41900479fbf4c2419a7f9ffb5acc1a2d77150d
renquanboMacbookPro <renquanbo7453@gmail.com>
2021-08-28 23:24:02 +1200

    add message page in student

--- pages/dashboard/student/message/index.tsx
+++ pages/dashboard/student/message/index.tsx
@@ -1,13 +1,1 @@
-import { Typography } from "antd";
-import AppLayout from "../../../../components/layout/AppLayout";
-
-const Message = () => {
-
-    return (
-        <AppLayout>
-            <Typography.Title>This is a student messages page</Typography.Title>
-        </AppLayout>
-    )
-}
-
-export default Message;
+export { default } from '../../manager/message';
